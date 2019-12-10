0362c4bf57aea40ae3ed75999caa7c823856c231
Stein Hammer <steihamm@gmail.com>
2019-12-10 21:30:17 +0100

    Minimal virker

--- bin/rss-reader.pl
+++ bin/rss-reader.pl
@@ -7,6 +7,8 @@ use SH::ScriptX;
 use Mojo::Base 'SH::ScriptX';
 use utf8;
 use open ':encoding(UTF-8)';
+use Mojo::Feed;
+use Data::Dumper;
 #use Carp::Always;
 
 =head1 NAME
@@ -20,9 +22,40 @@ rss-reader.pl - RSS Reader. Pick episode to download.
 =cut
 option 'dryrun!', 'Print to screen instead of doing changes';
 
+
  sub main {
     my $self = shift;
     my @e = $self->extra_options;
+    my @unwanted = qw/antipanel reprise trær/;
+    #my $old_date = Mojo::Date->new('2019-06-30T23:59:59+01:00')->epoch;
+    my $old_date = Mojo::Date->new('Wed, 03 Jul 2019 08:00:00 GMT')->epoch;
+    my $feed = Mojo::Feed->new(
+    	url => "https://podkast.nrk.no/program/ekko_-_et_aktuelt_samfunnsprogram.rss");
+    say $feed->title;
+    ITEM:
+    for my $item($feed->items->head(300)->each) {
+    	next if $old_date > Mojo::Date->new($item->published)->epoch;
+    	say "--";
+
+      	my $title = $item->title;
+      	for my $x(@unwanted) {
+      		next if $title =~ /$x/;
+      	}
+      	say $item->title;
+
+		my $description = $item->description;
+      	for my $x(@unwanted) {
+      		next ITEM if $description =~ /$x/;
+      	}
+		say $description;
+
+		my $url = $item->enclosures->to_array->[0];
+		$url =~ s/.*url=\"//;
+		$url =~ s/\".*//;
+		say "wget $url";
+
+		say  Mojo::Date->new($item->published);
+    }
 }
 
 __PACKAGE__->new(options_cfg=>{extra=>1})->main();
