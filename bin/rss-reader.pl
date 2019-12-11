72767f15a75de1ed04920e7a4263e4598211d941
Stein Hammer <steihamm@gmail.com>
2019-12-11 21:54:12 +0100

    slutten av dagen status

--- bin/rss-reader.pl
+++ bin/rss-reader.pl
@@ -29,33 +29,38 @@ option 'dryrun!', 'Print to screen instead of doing changes';
     my @unwanted = qw/antipanel reprise trær plante/;
     #my $old_date = Mojo::Date->new('2019-06-30T23:59:59+01:00')->epoch;
     my $old_date = Mojo::Date->new('Mon, 23 Sep 2019 10:30:00 GMT')->epoch;
-    # https://podkast.nrk.no/program/abels_taarn.rss 1/7-2019
-    my $feed = Mojo::Feed->new(
-    	url => "https://podkast.nrk.no/program/ekko_-_et_aktuelt_samfunnsprogram.rss");
-    say $feed->title;
-    ITEM:
-    for my $item($feed->items->head(300)->each) {
-    	next if $old_date > Mojo::Date->new($item->published)->epoch;
-    	say "--";
+    my @rsses = qw /https://podkast.nrk.no/program/ekko_-_et_aktuelt_samfunnsprogram.rss https://podkast.nrk.no/program/abels_taarn.rss/;
+    my @items;
+    for my $rss (@rsses) {
+    	my $feed = Mojo::Feed->new(
+    		url => $rss);
+    		my $item;
+	    say $feed->title;
+	    ITEM:
+	    for my $raw($feed->items->head(300)->each) {
+	    	next if $old_date > Mojo::Date->new($item->published)->epoch;
+	    	say "--";
 
-      	my $title = $item->title;
-      	for my $x(@unwanted) {
-      		next if $title =~ /$x/i;
-      	}
-      	say $item->title;
+	      	my $title = $item->title;
+	      	for my $x(@unwanted) {
+	      		next if $title =~ /$x/i;
+	      	}
+	      	say $raw->title;
 
-		my $description = $item->description;
-      	for my $x(@unwanted) {
-      		next ITEM if $description =~ /$x/i;
-      	}
-		say $description;
+			my $description = $raw->description;
+	      	for my $x(@unwanted) {
+	      		next ITEM if $description =~ /$x/i;
+	      	}
+			say $description;
 
-		my $url = $item->enclosures->to_array->[0];
-		$url =~ s/.*url=\"//;
-		$url =~ s/\".*//;
-		say "wget $url";
+			my $url = $raw->enclosures->to_array->[0];
+			$url =~ s/.*url=\"//;
+			$url =~ s/\".*//;
+			say "wget $url";
 
-		say  Mojo::Date->new($item->published);
+			say  Mojo::Date->new($raw->published);
+			push @items, $item;
+	    }
     }
 }
 
